e9a0ec7d03dcd3189004f7b795e176b1df363bba
ImJonahCodes <stjhoffman6@students.pghschools.org>
2024-03-04 04:44:15 -0500

    blog

--- src/queries/get-article-by-slug.js
+++ src/queries/get-article-by-slug.js
@@ -3,6 +3,11 @@ query ($slug: String) {
    Article (slug: $slug) {
      _id
      title
+     excerpt
+     cover_image {
+      _id
+      url
+     }
      content {
        __typename
        ... on Text {
