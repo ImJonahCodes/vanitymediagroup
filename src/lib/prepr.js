12e6259f4feaf350b488db6450b7c3973da6ac86
ImJonahCodes <stjhoffman6@students.pghschools.org>
2024-03-05 22:18:05 -0500

    ghjkl

--- src/lib/prepr.js
+++ src/lib/prepr.js
@@ -1,7 +1,7 @@
 // ./src/lib/prepr.js 
 
 export async function Prepr(query, variables) {
-    const response = await fetch(import.meta.env.PREPR_ENDPOINT, {
+    const response = await fetch("https://graphql.prepr.io/69a030b3ab0bb15e93420881336203a9dc34dce9e5781acdd576b495df7d153c", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
